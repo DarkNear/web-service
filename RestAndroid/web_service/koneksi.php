7ef83af7204a660b5ec41de2d9300502566c19bc
DarkNear <55751602+DarkNear@users.noreply.github.com>
2020-04-05 15:34:53 +0700

    app-with-api

---
+++ RestAndroid/web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id12789027_kita');
+ define('PASS','123456');
+ define('DB','id12789027_bajukita');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
